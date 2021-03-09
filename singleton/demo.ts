80dfd992354848cbb8fe4cc9e79f33f2f754e587
Nguyen Manh Hiep <nguyen.fs90@gmail.com>
2021-03-09 21:51:16 +0900

    singleton

---
+++ singleton/demo.ts
@@ -0,0 +1,14 @@
+namespace SingletonPattern {
+  export namespace Demo {
+    export function show(): void {
+      const singleton1 = SingletonPattern.Singleton.getInstance();
+      const singleton2 = SingletonPattern.Singleton.getInstance();
+
+      if (singleton1 === singleton2) {
+        console.log("two singletons are equivalent");
+      } else {
+        console.log("two singletons are not equivalent");
+      }
+    }
+  }
+}
